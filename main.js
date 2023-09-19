f96dc0e68f09230c8b211d0c8e63f612e92e2b03
Ryosuke Natori <nryosuke2001@gmail.com>
2023-09-20 01:54:48 +0900

    samplesを追加

--- main.js
+++ main.js
@@ -1,19 +1,22 @@
 // テストファイル: directoryTest.js
 
-const fs = require('fs');
-const path = require('path');
-const { testFunction } = require('./sampleSystem'); // システムのインポート
+import fs from 'fs';
+import path from 'path';
+// システムのインポート
 
-const testDirectory = '/path/to/your/directory'; // テストするディレクトリのパス
+const testDirectory =
+  '/home/ryosuke/project/reflected-and-stored-xss-test/samples'; // テストするディレクトリのパス
 
-describe('ディレクトリ内のファイルのテスト', () => {
-  const files = fs.readdirSync(testDirectory);
+// describe('ディレクトリ内のファイルのテスト', () => {
+const files = fs.readdirSync(testDirectory);
 
-  files.forEach((file) => {
-    test(`ファイル ${file} をテスト`, () => {
-      const filePath = path.join(testDirectory, file); // ファイルのフルパスを取得
-      const result = testFunction(filePath); // システムのテスト対象関数を呼び出す
-      expect(result).toBe(true); // 期待値がtrueであることをアサート
-    });
-  });
+files.forEach((file) => {
+  const filePath = path.join(testDirectory, file); // ファイルのフルパスを取得
+  console.log(filePath);
+  // test(`ファイル ${file} をテスト`, () => {
+  //   const filePath = path.join(testDirectory, file); // ファイルのフルパスを取得
+  //   const result = testFunction(filePath); // システムのテスト対象関数を呼び出す
+  //   expect(result).toBe(true); // 期待値がtrueであることをアサート
+  // });
 });
+// });
