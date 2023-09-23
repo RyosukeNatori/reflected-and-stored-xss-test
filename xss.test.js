cc13ffb29680f780c24a3c77d6a81890f8386af6
Ryosuke Natori <nryosuke2001@gmail.com>
2023-09-24 01:10:46 +0900

    タイポ修正

--- xss.test.js
+++ xss.test.js
@@ -14,12 +14,12 @@ export const makeTest = ({ dirName, testName }) => {
     const files = fs.readdirSync(dirName);
 
     files.forEach((file) => {
-      exprectAssertXSS({ filePath: path.join(dirName, file) });
+      expectAssertXSS({ filePath: path.join(dirName, file) });
     });
   });
 };
 
-const exprectAssertXSS = async ({ filePath }) => {
+const expectAssertXSS = async ({ filePath }) => {
   test(`ファイル ${filePath} をテスト`, async () => {
     expect(await assertXSS({ filePath })).toBe(true);
   });
